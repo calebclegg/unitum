4af75f13e7b82df1a678d3967fdd8fed09d0a62d
Dchole <derekoware47@gmail.com>
2022-01-07 09:19:20 +0000

    replace bcrypt with bcryptjs

--- backend/src/types/post.ts
+++ backend/src/types/post.ts
@@ -1,16 +1,16 @@
 import { Types } from "mongoose";
 
 interface IComment {
-    userID: Types.ObjectId,
-    postID: Types.ObjectId,
-    text: string,
-    createdAt?: Date
+  userID: Types.ObjectId;
+  postID: Types.ObjectId;
+  text: string;
+  createdAt?: Date;
 }
 interface IPost {
-    userID: Types.ObjectId,
-    communityID: Types.ObjectId,
-    text: string,
-    numberOfComments?: number
-    comments?: [IComment]
-    upvotes?: Number
+  userID: Types.ObjectId;
+  communityID: Types.ObjectId;
+  text: string;
+  numberOfComments?: number;
+  comments?: [IComment];
+  upvotes?: Number;
 }
