d82fcd9feb4edd8b26b49879ede8193c05c10bde
Dchole <derekoware47@gmail.com>
2022-01-07 09:44:22 +0000

    import order

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import * as controller from "../controllers/user";
+import {
+  validateUserRegData,
+  validateUserLogData
+} from "../middlewares/user.middleware";
+
 const router = Router();
-import {validateUserRegData, validateUserLogData} from "../middlewares/user.middleware"
 //set route and it's controller
 router.post("/register", validateUserRegData, controller.register);
 
-router.post("/login", validateUserLogData, controller.login)
+router.post("/login", validateUserLogData, controller.login);
 
-router.post("/authProvider", controller.checkAuthProvider)
+router.post("/authProvider", controller.checkAuthProvider);
 
-router.post("/auth", controller.externalAuth)
+router.post("/auth", controller.externalAuth);
 
 export default router;
