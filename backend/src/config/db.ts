d565bc050c3bae46b8d8add404aa2f80a1e27c9d
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-02-01 22:16:22 +0000

    Chore: Prepare for deployments

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,4 +1,4 @@
-import mongoose, { connection } from "mongoose";
+import mongoose from "mongoose";
 const connectDB = async () => {
   const NODE_ENV = process.env.NODE_ENV;
   try {
--- backend/src/config/redis_connect.ts
+++ backend/src/config/redis_connect.ts
@@ -1,15 +1,20 @@
-import { Client, Schema, Entity } from "redis-om";
+import { Client } from "redis-om";
 
 export const redisClient = new Client();
 
 export const redisConnect = async () => {
+  let limit = 5;
   if (!redisClient.isOpen()) {
     try {
       await redisClient.open(process.env.REDIS_URI);
       console.log("Redis server running...");
     } catch (error) {
-      console.log("Unable to connect to redis sever!!\n Reconnecting...");
-      await redisConnect();
+      while (limit > 0) {
+        console.log("Unable to connect to redis sever!!\n Reconnecting...");
+        await redisConnect();
+        limit--;
+        console.log(limit);
+      }
     }
   }
 };
