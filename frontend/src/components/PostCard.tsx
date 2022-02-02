5cb9de15dcb150bed42370a3f654786c9ce681a2
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-02-02 04:26:34 +0000

    Fix: Add missing close brace to Postcard.tsx

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -70,7 +70,6 @@ const PostCard = ({
           }
         });
 
-
         setVote((prevState) => ({
           upVote: !prevState.upVote,
           downVote: false
@@ -82,111 +81,108 @@ const PostCard = ({
           }
         });
 
-      revalidate?.();
-
+        revalidate?.();
+      }
     }
-  };
 
-  return (
-    <Card variant="outlined" sx={{ p: tabletUp ? 1 : 0 }}>
-      <CardHeader
-        avatar={
-          <Avatar
-            src={author?.profile.picture}
-            alt={author?.profile.fullName}
-            sx={{ bgcolor: "rgb(255, 0, 0)" }}
-          />
-
-        }
-        action={
-          <IconButton
-            sx={{ color: ({ customPalette }) => customPalette.navyBlue }}
-            aria-label={saved ? "remove post from saved" : "save post"}
-            onClick={toggleSaved}
-          >
-            {saved ? <Bookmark /> : <BookmarkOutlined />}
-          </IconButton>
-        }
-
-        title={author?.profile.fullName}
-        subheader={
-          <>
-            {communityID?.name && (
-              <>
-                <span
-                  style={
-                    tabletUp
-                      ? undefined
-                      : {
-                          width: 80,
-                          whiteSpace: "nowrap",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis"
-                        }
-                  }
-                >
-                  {communityID.name}
-                </span>
-                <span>&bull;</span>
-              </>
-            )}
-            <span>{createdAt && new Date(createdAt).toDateString()}</span>
-
-          </>
-        }
-        titleTypographyProps={{ fontWeight: 500, variant: "h6" }}
-        subheaderTypographyProps={{ sx: { display: "flex", gap: 1 } }}
-      />
-      <Stack direction="row" ml={2}>
-        <Stack alignItems="center">
-          <IconButton
-            size="small"
-            color={vote.upVote ? "secondary" : "default"}
-            aria-label="up vote"
-            data-vote="upVote"
-            onClick={addVote}
-          >
-            <Forward fontSize="small" transform="rotate(-90)" />
-          </IconButton>
-          <Typography variant="caption">{upvotes}</Typography>
-          <IconButton
-            size="small"
-            color={vote.downVote ? "secondary" : "default"}
-            aria-label="down vote"
-            data-vote="downVote"
-            onClick={addVote}
-          >
-            <Forward fontSize="small" transform="rotate(90)" />
-          </IconButton>
-        </Stack>
-        <div>
-          <CardContent sx={{ py: 0.5 }}>
-            <MuiLink
-              component={Link}
-              to={`/feed/${_id}`}
-              color="textSecondary"
-              underline="none"
+    return (
+      <Card variant="outlined" sx={{ p: tabletUp ? 1 : 0 }}>
+        <CardHeader
+          avatar={
+            <Avatar
+              src={author?.profile.picture}
+              alt={author?.profile.fullName}
+              sx={{ bgcolor: "rgb(255, 0, 0)" }}
+            />
+          }
+          action={
+            <IconButton
+              sx={{ color: ({ customPalette }) => customPalette.navyBlue }}
+              aria-label={saved ? "remove post from saved" : "save post"}
+              onClick={toggleSaved}
             >
-              {body}
-            </MuiLink>
-          </CardContent>
-          <CardActions>
-            <Button
-              variant="text"
-              component={Link}
-              to={`/feed/${_id}#comments`}
-            >
-              {numberOfComments ? (
-                <>Comments ({numberOfComments})</>
-              ) : (
-                <>No comments yet</>
+              {saved ? <Bookmark /> : <BookmarkOutlined />}
+            </IconButton>
+          }
+          title={author?.profile.fullName}
+          subheader={
+            <>
+              {communityID?.name && (
+                <>
+                  <span
+                    style={
+                      tabletUp
+                        ? undefined
+                        : {
+                            width: 80,
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis"
+                          }
+                    }
+                  >
+                    {communityID.name}
+                  </span>
+                  <span>&bull;</span>
+                </>
               )}
-            </Button>
-          </CardActions>
-        </div>
-      </Stack>
-    </Card>
-  );
+              <span>{createdAt && new Date(createdAt).toDateString()}</span>
+            </>
+          }
+          titleTypographyProps={{ fontWeight: 500, variant: "h6" }}
+          subheaderTypographyProps={{ sx: { display: "flex", gap: 1 } }}
+        />
+        <Stack direction="row" ml={2}>
+          <Stack alignItems="center">
+            <IconButton
+              size="small"
+              color={vote.upVote ? "secondary" : "default"}
+              aria-label="up vote"
+              data-vote="upVote"
+              onClick={addVote}
+            >
+              <Forward fontSize="small" transform="rotate(-90)" />
+            </IconButton>
+            <Typography variant="caption">{upvotes}</Typography>
+            <IconButton
+              size="small"
+              color={vote.downVote ? "secondary" : "default"}
+              aria-label="down vote"
+              data-vote="downVote"
+              onClick={addVote}
+            >
+              <Forward fontSize="small" transform="rotate(90)" />
+            </IconButton>
+          </Stack>
+          <div>
+            <CardContent sx={{ py: 0.5 }}>
+              <MuiLink
+                component={Link}
+                to={`/feed/${_id}`}
+                color="textSecondary"
+                underline="none"
+              >
+                {body}
+              </MuiLink>
+            </CardContent>
+            <CardActions>
+              <Button
+                variant="text"
+                component={Link}
+                to={`/feed/${_id}#comments`}
+              >
+                {numberOfComments ? (
+                  <>Comments ({numberOfComments})</>
+                ) : (
+                  <>No comments yet</>
+                )}
+              </Button>
+            </CardActions>
+          </div>
+        </Stack>
+      </Card>
+    );
+  };
 };
 
 export default PostCard;
