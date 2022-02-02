d5c97404d45ad957ae50feadf1bd11c401c823fb
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-02-02 03:59:42 +0000

    Chore: update scripts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -59,10 +59,10 @@ redisConnect();
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "../../frontend/build")));
   // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
   });
 }
 //Body parser setup
