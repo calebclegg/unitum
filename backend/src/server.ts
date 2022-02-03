d7e684c465cdcda87520f0ad2027994459ea4635
Dchole <derekoware47@gmail.com>
2022-02-03 03:22:08 +0000

    create frontend v2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -60,10 +60,10 @@ redisConnect();
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "../../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../../frontendv2/build")));
   // Handle React routing, return all requests to React app
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontendv2/build", "index.html"));
   });
 }
 //Body parser setup
