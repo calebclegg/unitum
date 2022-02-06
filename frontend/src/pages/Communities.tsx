eedb68b3f57b676f5f54c6e3d0030c510ef0b819
Dchole <derekoware47@gmail.com>
2022-02-06 23:01:41 +0000

    communities activities

--- frontend/src/pages/Communities.tsx
+++ frontend/src/pages/Communities.tsx
@@ -1,3 +1,4 @@
+import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import PostCard, { IProps as IPost } from "../components/PostCard";
@@ -8,11 +9,30 @@ const Communities = () => {
   const { toggleSave, toggleVote } = usePostsActions(posts, mutate);
 
   return (
-    <Stack spacing={4} component="section" id="communities-activities">
-      <Typography variant="h4" component="h1">
-        Communities Activities
-      </Typography>
-      <Stack spacing={2}>
+    <>
+      <Paper
+        square
+        variant="outlined"
+        sx={{
+          px: 3,
+          py: 1.5,
+          maxWidth: ({ breakpoints }) => breakpoints.values.md
+        }}
+      >
+        <Typography variant="h5" fontWeight={500} component="h1">
+          Communities Activities
+        </Typography>
+      </Paper>
+      <Stack
+        spacing={2}
+        maxWidth={({ breakpoints }) => breakpoints.values.md}
+        sx={{
+          "& .MuiPaper-rounded:first-of-type": {
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0
+          }
+        }}
+      >
         {posts?.map((post) => (
           <PostCard
             {...post}
@@ -22,7 +42,7 @@ const Communities = () => {
           />
         ))}
       </Stack>
-    </Stack>
+    </>
   );
 };
 
