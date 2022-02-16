c762cfb438f99f58a0ba6ffef47ba137268b4d38
Dchole <derekoware47@gmail.com>
2022-02-16 19:42:11 +0000

    upload community avatar

--- frontend/src/pages/Community.tsx
+++ frontend/src/pages/Community.tsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Edit from "@mui/icons-material/Edit";
+import Check from "@mui/icons-material/Check";
+import Close from "@mui/icons-material/Close";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
@@ -11,17 +15,21 @@ import Typography from "@mui/material/Typography";
 import PostCard, { IProps as IPost } from "../components/PostCard";
 import { ICommunity, useData, usePostsActions, useUser } from "../hooks";
 import { API } from "../lib";
-import { lazy, Suspense, useState } from "react";
+import { lazy, Suspense, useRef, useState } from "react";
 import { useAuth } from "../context/Auth";
 
 const ConfirmDialog = lazy(() => import("../components/ConfirmDialog"));
 
 const Community = () => {
+  const uploadFileRef = useRef<HTMLInputElement>(null);
   const { comm_id } = useParams<{ comm_id: string }>();
   const { user } = useUser();
   const { token } = useAuth();
+  const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
   const [sendingJoinRequest, setSendingJoinRequest] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
   const { data: community } = useData<ICommunity>(`community/${comm_id}`);
   const { data: posts, mutate } = useData<IPost[]>(
     `posts?communityID=${comm_id}`
@@ -47,6 +55,70 @@ const Community = () => {
     }
   };
 
+  const uploadImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+
+    if (files) {
+      const [file] = files;
+      setFile(file);
+      const reader = new FileReader();
+
+      reader.addEventListener("load", (event) => {
+        if (event.target?.result) setPreview(event.target.result);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const removePreview = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setPreview(null);
+    if (uploadFileRef.current) uploadFileRef.current.value = "";
+    event.currentTarget.blur();
+  };
+
+  const savePreview = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    try {
+      setUploading(true);
+
+      // Upload preview
+      const uploadData = new FormData();
+      if (file) {
+        uploadData.append("media", file);
+      }
+
+      const {
+        data: [imageURL]
+      } = await API.post<string[]>("uploads", uploadData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data"
+        }
+      });
+
+      // Update community
+      const { data } = await API.patch(
+        `community/${comm_id}`,
+        {
+          picture: imageURL
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+
+      console.log({ data });
+      await mutate();
+      removePreview(event);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
   const openConfirmDialog = () => setIsDialogOpen(true);
   const closeConfirmDialog = () => setIsDialogOpen(false);
 
@@ -68,16 +140,118 @@ const Community = () => {
           <Edit fontSize="small" />
         </IconButton>
         <Grid container spacing={2}>
-          <Grid item>
+          <Grid
+            item
+            width={104}
+            height={104}
+            position="relative"
+            borderRadius={2}
+            bgcolor="#cacaca68"
+            sx={{
+              transition: ({ transitions }) =>
+                transitions.create("background-color", {
+                  duration: transitions.duration.shortest
+                }),
+
+              "&.MuiGrid-item": {
+                pl: 0,
+                pt: 0
+              },
+
+              "&:hover, &:focus-within": {
+                bgcolor: "#121212AA",
+
+                "& .MuiIconButton-root": {
+                  opacity: 1
+                },
+
+                "& .MuiAvatar-root": {
+                  opacity: 0.15
+                }
+              }
+            }}
+          >
+            {uploading ? (
+              <Stack
+                width="100%"
+                height="100%"
+                justifyContent="center"
+                alignItems="center"
+                position="absolute"
+                zIndex={({ zIndex }) => zIndex.mobileStepper}
+              >
+                <CircularProgress size={25} />
+              </Stack>
+            ) : preview ? (
+              <Stack
+                width="100%"
+                height="100%"
+                direction="row"
+                alignItems="center"
+                justifyContent="space-evenly"
+                top="50%"
+                left="50%"
+                position="absolute"
+                zIndex={({ zIndex }) => zIndex.mobileStepper}
+                sx={{
+                  transform: "translate(-50%, -50%)",
+
+                  "& .MuiIconButton-root": {
+                    bgcolor: "#fffd",
+                    "&:hover": {
+                      bgcolor: "#fff"
+                    }
+                  }
+                }}
+              >
+                <IconButton size="small" color="error" onClick={removePreview}>
+                  <Close fontSize="small" />
+                </IconButton>
+                <IconButton size="small" color="primary" onClick={savePreview}>
+                  <Check fontSize="small" />
+                </IconButton>
+              </Stack>
+            ) : (
+              <IconButton
+                sx={{
+                  color: "grey.100",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  opacity: 0,
+                  transform: "translate(-50%, -50%)",
+                  transition: ({ transitions }) =>
+                    transitions.create("opacity", {
+                      duration: transitions.duration.shortest
+                    }),
+                  zIndex: ({ zIndex }) => zIndex.mobileStepper
+                }}
+                onClick={() => uploadFileRef.current?.click()}
+              >
+                <PhotoCamera />
+              </IconButton>
+            )}
+            <input
+              ref={uploadFileRef}
+              onChange={uploadImage}
+              type="file"
+              name="avatar"
+              id="community-avatar"
+              accept="image/*"
+              hidden
+            />
             <Avatar
               variant="rounded"
-              sx={{
-                width: 94,
-                height: 94,
-                backgroundColor: "#cacaca68"
-              }}
-              src={community?.picture}
+              src={preview?.toString() || community?.picture}
               alt=""
+              sx={{
+                width: "inherit",
+                height: "inherit",
+                transition: ({ transitions }) =>
+                  transitions.create("opacity", {
+                    duration: transitions.duration.shortest
+                  })
+              }}
             />
           </Grid>
           <Grid item spacing={2}>
