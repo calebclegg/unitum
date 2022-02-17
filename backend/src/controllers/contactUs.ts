3d317ad9646962d99324a5b8909ca5cb05d8ffb7
Kyei Samuel <kyeisamuel931@gmail.com>
2022-02-17 12:45:51 +0000

    seeder script updated

--- backend/src/controllers/contactUs.ts
+++ backend/src/controllers/contactUs.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { Contact } from "../models/ContactUs";
 import { validateContactCreateData } from "../validators/contact.validator";
 
