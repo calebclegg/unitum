fa9af527696d60f257abd206355d5a250020fbbc
Dchole <derekoware47@gmail.com>
2022-02-17 19:02:28 +0000

    chat interface

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -10,10 +10,27 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/system/Box";
 import { Link } from "react-router-dom";
-import { useOpenWithHash } from "../hooks";
+import { useData, useOpenWithHash } from "../hooks";
+
+export interface IMessage {
+  _id: string;
+  chatID: string;
+  createdAt: string;
+  from: {
+    email: string;
+    profile: {
+      fullName: string;
+      picture: string;
+      _id: string;
+    };
+  };
+  read: boolean;
+  text: string;
+}
 
 const Notification = () => {
   const { open, handleClose } = useOpenWithHash("/chat");
+  const { data: messages } = useData<IMessage[]>("/chat/messages/unread");
 
   const goBack = (event: React.MouseEvent<HTMLAnchorElement>) => {
     if (window.history.length > 2) {
@@ -39,40 +56,46 @@ const Notification = () => {
       </Typography>
       <Stack direction="row" width="min(100%, 350px)">
         <List sx={{ width: "100%" }}>
-          <ListItem
-            component={Link}
-            to={`/chat/chat-id`}
-            sx={{ p: 0, alignItems: "flex-start" }}
-          >
-            <Stack direction="row" flexGrow={1} alignItems="center">
-              <ListItemAvatar>
-                <Avatar>U</Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary="John Doe"
-                secondary="Text message"
-                primaryTypographyProps={{ fontWeight: 500 }}
-              />
-            </Stack>
-            <Stack alignItems="flex-end" spacing={0.5}>
-              <Typography variant="caption" sx={{ mt: 1 }}>
-                12 mins ago
-              </Typography>
-              <Typography
-                variant="caption"
-                sx={{
-                  width: 18,
-                  height: 18,
-                  borderRadius: "50%",
-                  bgcolor: "secondary.light",
-                  display: "grid",
-                  placeItems: "center"
-                }}
-              >
-                4
-              </Typography>
-            </Stack>
-          </ListItem>
+          {messages?.map((message) => (
+            <ListItem
+              key={message._id}
+              component={Link}
+              to={`/chat/${message.chatID}`}
+              sx={{ p: 0, mb: 2, alignItems: "flex-start" }}
+            >
+              <Stack direction="row" flexGrow={1} alignItems="center">
+                <ListItemAvatar>
+                  <Avatar
+                    src={message.from.profile.picture}
+                    alt={message.from.profile.fullName}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={message.from.profile.fullName}
+                  secondary={message.text}
+                  primaryTypographyProps={{ fontWeight: 500 }}
+                />
+              </Stack>
+              <Stack alignItems="flex-end" spacing={0.5}>
+                <Typography variant="caption" sx={{ mt: 1 }}>
+                  12 mins ago
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{
+                    width: 18,
+                    height: 18,
+                    borderRadius: "50%",
+                    bgcolor: "secondary.light",
+                    display: "grid",
+                    placeItems: "center"
+                  }}
+                >
+                  4
+                </Typography>
+              </Stack>
+            </ListItem>
+          ))}
         </List>
       </Stack>
     </Dialog>
